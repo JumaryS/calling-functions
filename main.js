4939fdf891ec22151a57b4752d5cb8be83783959
JumarySanchez <sanchezjumary@gmail.com>
2020-06-08 09:48:25 -0400

    calling-functions homework

---
+++ main.js
@@ -0,0 +1,36 @@
+function addQuestionMarks(str){
+    console.log(str + "???") 
+}
+addQuestionMarks("hi")
+addQuestionMarks("what even")
+
+function addTwoStrings(str1,str2){
+    console.log(str1+ ' ' +str2)
+    console.log(str1+ ' ' +str2)
+}
+addTwoStrings('oh','hi')
+addTwoStrings('well','hello back')
+
+function subtractThreeNumbers(num1,num2,num3){
+ console.log(num1 -num2-num3)
+}
+subtractThreeNumbers(10,1,2)
+subtractThreeNumbers(8,3,5)
+subtractThreeNumbers(100,80,25)
+subtractThreeNumbers(2,1,10)
+subtractThreeNumbers(10,1,2)
+
+function tripleString(str){
+    console.log(str+str+str)
+}
+tripleString('codeimmersives')
+// 10, 1, 2 -> 7
+// 8, 3, 5 -> 0
+// 100, 80, 25 -> -5
+
+function squareRoot(num){
+    console.log(Math. sqrt(num))
+}
+squareRoot(9)
+squareRoot(16)
+squareRoot(4)
